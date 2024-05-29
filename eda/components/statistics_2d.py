a0576ce47512fe8f89068aa640da8e5c9355606c
marti3 <marcioch.s.m@gmail.com>
2024-05-29 17:40:38 +0200

    Basic 2D statistics, tables to fix

---
+++ eda/components/statistics_2d.py
@@ -0,0 +1,132 @@
+import math
+from io import StringIO
+
+from dash import dcc, html, callback, Input, Output, State, dash_table, no_update
+from dash.exceptions import PreventUpdate
+import dash_ag_grid as dag
+
+from eda.destats import *
+from eda.data_table.column_type import is_number_type
+
+
+def register_2d_stats_callbacks():
+
+    @callback(
+        Output('statistic_2d_output', 'children', allow_duplicate=True),
+        Input('dataframe', 'data'),
+        prevent_initial_call=True
+    )
+    def render(df_json: str):
+
+        return html.Div(id="stats-2d", children=[
+            html.H2("Statystki opisowe 2D"),
+            html.Div(id="stats-2d__dropdown"),
+            html.Div(id="stats-2d__selected-variable"),
+            html.Div(id="stats-2d__summary"),
+            html.Div(id="stats-2d__tables"),
+        ])
+
+    @callback(
+        Output('stats-2d__dropdown', 'children'),
+        Input('data-table', 'data'),
+        prevent_initial_call=True
+    )
+    def update_dropdowns(data_table):
+        df = pd.DataFrame(data_table)
+        options = [{'label': col, 'value': col} for col in df.columns]
+        dropdowns = [html.Div([
+            html.Label(f'Wybierz kolumny, dla których chcesz obliczyć statystyki: '),
+            dcc.Dropdown(options=options, id='2d-dropdown1')
+        ]),
+            html.Div([
+                dcc.Dropdown(options=options, id='2d-dropdown2')
+            ])]
+
+        return dropdowns
+
+    @callback(
+        Output('stats-2d__selected-variable', 'children'),
+        Output('stats-2d__summary', 'children'),
+        [Input('2d-dropdown1', 'value'),
+         Input('2d-dropdown2', 'value')],
+        State('data-table', 'data'),
+        State('stored-dtypes', 'data'),
+        prevent_initial_call=True
+    )
+    def computing_stats(x, y, data, dtypes):
+        if x is None or y is None:
+            raise PreventUpdate
+
+        info = f"Aktualnie wybrane wartości: {x}, {y}"
+        df = pd.DataFrame(data)
+
+        if is_number_type(dtypes[x]) and is_number_type(dtypes[y]):
+            return info, numeric_stats(df, x, y)
+        else:
+            return info, no_update
+
+    def numeric_stats(data, x, y):
+        labels = [
+            "Korelacja Pearsona",
+            "Korelacja Spearmana",
+            "Współczynnik determinacji",
+            "Regresja liniowa",
+            "Obszar ufności",
+            "Współczynnik korelacji",
+        ]
+        x, y = clean_columns(data[x], data[y])
+        functions = [
+            pearson_correlation(x, y),
+            spearman_correlation(x, y),
+            coefficient_of_determination(x, y),
+            linear_regression(x, y),
+            confidence_interval(x, y),
+            correlation_coefficient(x, y),
+        ]
+
+        stats = [
+            html.Div([
+                html.H3(label + ":"),
+                html.Pre(function)
+            ])
+            for label, function in zip(labels, functions)
+        ]
+
+        return stats
+
+    @callback(
+        Output('stats-2d__tables', 'children'),
+        [Input('2d-dropdown1', 'value'),
+         Input('2d-dropdown2', 'value')],
+        State('data-table', 'data'),
+        State('stored-dtypes', 'data'),
+        prevent_initial_call=True
+    )
+    def generate_tables(x, y, data, dtypes):
+        if data is None or x is None or y is None:
+            raise PreventUpdate
+
+        df = pd.DataFrame(data)
+
+        if is_number_type(dtypes[x]) and is_number_type(dtypes[y]):
+            return html.Div([dcc.Markdown(numeric_tables(df, x, y))])
+        return no_update
+
+    def numeric_tables(data, x, y):
+        new_data = data.loc[:, [x, y]]
+        new_data.columns = [x, y]
+        result = [
+            anova_analysis(data, f"{y} ~ {x}"),
+            ancova_analysis(data, f"{y} ~ {x}"),
+            cluster_analysis(new_data, 2),
+        ]
+        df = pd.DataFrame(result)
+        return dash_table.DataTable(
+            id="numeric-tables",
+            columns=[
+                {"name": col, "id": col, "deletable": True, "renamable": True}
+                for col in df.columns.tolist()
+            ],
+            data=df.to_dict("records"),
+            page_size=15,
+        )
