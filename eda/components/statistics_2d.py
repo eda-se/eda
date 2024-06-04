7cbf6493235abfe57a3f81a41314a1aaaaf5403d
marti3 <marcioch.s.m@gmail.com>
2024-06-05 01:17:49 +0200

    Categorical tables fix

--- eda/components/statistics_2d.py
+++ eda/components/statistics_2d.py
@@ -7,6 +7,9 @@ from eda.data_table.column_type import is_number_type, is_categorical_type
 
 
 def register_2d_stats_callbacks():
+    MAX_ROWS = 15
+    ROW_HEIGHT = 42
+    HEADER_ROW_HEIGHT = 49
     @callback(
         Output('statistic_2d_output', 'children', allow_duplicate=True),
         Input('dataframe', 'data'),
@@ -16,7 +19,6 @@ def register_2d_stats_callbacks():
         return html.Div(id="stats-2d", children=[
             html.H2("Statystki opisowe 2D"),
             html.Div(id="stats-2d__dropdown"),
-            html.Div(id="stats-2d__selected-variable"),
             html.Div(id="stats-2d__summary"),
             html.Div(id="stats-2d__tables"),
         ])
@@ -40,7 +42,6 @@ def register_2d_stats_callbacks():
         return dropdowns
 
     @callback(
-        Output('stats-2d__selected-variable', 'children'),
         Output('stats-2d__summary', 'children'),
         Input('2d-dropdown1', 'value'),
         Input('2d-dropdown2', 'value'),
@@ -52,15 +53,14 @@ def register_2d_stats_callbacks():
         if x is None or y is None:
             raise PreventUpdate
 
-        info = f"Aktualnie wybrane wartości: {x}, {y}"
         df = pd.DataFrame(data)
 
         if is_number_type(dtypes[x]) and is_number_type(dtypes[y]):
-            return info, numeric_stats(df, x, y)
+            return numeric_stats(df, x, y)
         if is_categorical_type(dtypes[x]) and is_number_type(dtypes[y]):
-            return info, create_statistics_table(df, x, y)
+            return create_categorical_tables(df, x, y)
 
-        return info, "Wybierz albo dwie wartości numeryczne, albo pierwszą kategoryczną, a drugą numeryczną!"
+        return None
 
     def numeric_stats(data, x, y):
         labels = [
@@ -102,45 +102,61 @@ def register_2d_stats_callbacks():
 
         return stats
 
-    def categorical_stats(data, x, y):
-        formula = f'{y} ~ {x}'
-        n_clusters = 2
-        anova_results = anova_analysis(data, formula)
-        ancova_results = ancova_analysis(data, formula)
-        regression_predictions = linear_regression_analysis(data[x], data[y])
-        cluster_labels = cluster_analysis(data, n_clusters)
+    def create_categorical_tables(data, x, y):
+        x_, y_ = clean_columns(data[x], data[y])
+        cluster = cluster_analysis(pd.DataFrame({x: x_, y: y_}), 2)
+        regression = linear_regression_analysis(x_, y_)
+        df = data.rename(columns={x: 'A'})
+        df = df.rename(columns={y: 'B'})
+        formula = 'B ~ A'
+        anova = anova_analysis(df, formula)
+        ancova = ancova_analysis(df, formula)
 
-        combined_results = {
-            "ANOVA": anova_results.to_dict(),
-            "ANCOVA": ancova_results.to_dict(),
-            "Regression Predictions": regression_predictions.to_dict(),
-            "Cluster Labels": cluster_labels.to_dict()
-        }
-
-        combined_df = pd.concat(
-            [pd.DataFrame.from_dict(v, orient='index').reset_index() for k, v in combined_results.items()],
-            keys=combined_results.keys(),
-            names=['Analysis Type', 'Index']
-        ).reset_index()
-
-        return combined_df
-
-    def create_statistics_table(data: pd.DataFrame, x: pd.Series, y: pd.Series):
-        statistics_df = categorical_stats(data, x, y, )
-
-        column_defs = [
-            {"field": "Analysis Type", "headerName": "Type of Analysis"},
-            {"field": "Index", "headerName": "Index"},
-            {"field": "index", "headerName": "Statistic"},
-            {"field": 0, "headerName": "Value"}
-        ]
-
-        return dag.AgGrid(
-            id="statistics-table",
-            rowData=statistics_df.to_dict("records"),
-            columnDefs=column_defs,
+        anova_table = dag.AgGrid(
+            id="stats-anova-table",
+            rowData=anova.to_dict("records"),
+            columnDefs=[
+                {"field": "df", "headerName": "Stopnie swobody"},
+                {"field": "sum_sq", "headerName": "Suma kwadratów"},
+                {"field": "mean_sq", "headerName": "Średnia kwadratów"},
+                {"field": "F", "headerName": "Wartość F"},
+                {"field": "PR(>F)", "headerName": "p-wartość"},
+            ],
             style={
-                "height": min(statistics_df.shape[0], 20) * 28 + 30 + 2,
-                "overflow": "hidden" if statistics_df.shape[0] <= 20 else "auto"
+                "height": min(anova.shape[0], MAX_ROWS) * ROW_HEIGHT + HEADER_ROW_HEIGHT + 2,
+                "overflow": "hidden" if anova.shape[0] <= MAX_ROWS else "auto"
             }
         )
+
+        ancova_table = dag.AgGrid(
+            id="stats-ancova-table",
+            rowData=ancova.to_dict("records"),
+            columnDefs=[
+                {"field": "df", "headerName": "Stopnie swobody"},
+                {"field": "sum_sq", "headerName": "Suma kwadratów"},
+                {"field": "F", "headerName": "Wartość F"},
+                {"field": "PR(>F)", "headerName": "p-wartość"},
+            ],
+            style={
+                "height": min(ancova.shape[0], MAX_ROWS) * ROW_HEIGHT + HEADER_ROW_HEIGHT + 2,
+                "overflow": "hidden" if ancova.shape[0] <= MAX_ROWS else "auto"
+            }
+        )
+
+        combined_table = pd.concat([regression, cluster], axis=1)
+
+        cluster_regression_table = dag.AgGrid(
+            id="combined-results",
+            rowData=combined_table.reset_index().to_dict("records"),
+            columnDefs=[
+                {"field": "index", "headerName": "Index"},
+                {"field": "0", "headerName": "Przewidziane wyniki regresji"},
+                {"field": "1", "headerName": "Klaster"},
+            ],
+            style={
+                "height": min(combined_table.shape[0], MAX_ROWS) * ROW_HEIGHT + HEADER_ROW_HEIGHT + 2,
+                "overflow": "hidden" if combined_table.shape[0] <= MAX_ROWS else "auto"
+            }
+        )
+
+        return html.H3("Analiza wariancji"), anova_table, html.H3("Analiza kowariancji"), ancova_table, html.H3("Analiza regresji i klasteryzacja"), cluster_regression_table
