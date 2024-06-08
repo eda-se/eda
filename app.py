787ef3c19871c2f0b636eb31de259435c9ca6956
Jakub Szaredko <jakub.szaredko@gmail.com>
2024-06-09 00:03:25 +0200

    Fix deployment

--- app.py
+++ app.py
@@ -46,39 +46,30 @@ index_template = """
     </html>
 """
 
+app = Dash(__name__,
+    title="EDA",
+    index_string=index_template,
+    suppress_callback_exceptions=True
+)
 
-def run():
-    app = Dash(__name__,
-        title="EDA",
-        index_string=index_template,
-        suppress_callback_exceptions=True
-    )
+app.layout = html.Div(id="main", children=[
+    H1("Analiza Eksploracyjna"),
+    html.Div(id="output", className="py-6"),
 
-    app.layout = html.Div(id="main", children=[
-        H1("Analiza Eksploracyjna"),
-        html.Div(id="output", className="py-6"),
+    html.Div(id="data_correction_output", className="py-6"),
+    html.Div(id="statistic_output", className="py-6"),
+    html.Div(id="statistic_2d_output", className="py-6"),
+    # html.Div(id="graph_output", className="py-6"),
+    html.Footer(className="h-40"),
 
-        html.Div(id="data_correction_output", className="py-6"),
-        html.Div(id="statistic_output", className="py-6"),
-        html.Div(id="statistic_2d_output", className="py-6"),
-        # html.Div(id="graph_output", className="py-6"),
-        html.Footer(className="h-40"),
+    dcc.Store(id="dataframe"),
+])
 
-        dcc.Store(id="dataframe"),
-    ])
+register_input_callbacks()
+register_dataframe_callbacks()
+register_1d_stats_callbacks()
+register_2d_stats_callbacks()
+# register_graph_callbacks()
+register_data_correction_callbacks()
 
-    register_input_callbacks()
-    register_dataframe_callbacks()
-    register_1d_stats_callbacks()
-    register_2d_stats_callbacks()
-    # register_graph_callbacks()
-    register_data_correction_callbacks()
-
-    if DEVELOPMENT:
-        app.run(debug=True)
-    else:
-        app.run(host="0.0.0.0")
-
-
-if __name__ == '__main__':
-    run()
+server = app.server
